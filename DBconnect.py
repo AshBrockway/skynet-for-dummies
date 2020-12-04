692fa9e41c3035f8191e35ee6b4bd8c988d285fd
andrew.reilly19 <andrew.reilly19@ncf.edu>
2020-12-03 23:37:06 -0500

    Final (?) update to DBconnect
    bugs now ironed out, working smoothly on local machine and no reason to suspect issues on server

--- DBconnect.py
+++ DBconnect.py
@@ -14,6 +14,28 @@ Database layout:
 
 Using a schema as the one described above will allow our dashboard to have two stages - a (potentially live) visualization of the model as it is running,
 and a comparison view showing comparisons of models (over time)
+
+skynet_params is the 'master' table
+
+Creation query:
+
+    CREATE TABLE skynet_params" (
+    "iteration" int,
+    "resources" decimal,
+    "syscap" decimal,
+    "long" decimal,
+    "short" decimal,
+    "resourcelow" decimal,
+    "resourcehigh" decimal,
+    "timelow" decimal,
+    "timehigh" decimal,
+    "jobs" decimal,
+    "dpn_loss" decimal,
+    "dpcnn_loss" decimal,
+    "ac_loss" decimal,
+    "fifo_loss" decimal,
+    "sjf_loss" decimal,
+
 """
 
 import psycopg2
@@ -52,9 +74,9 @@ class DBconnect:
         self.iter = cur_iter
 
         #creating iteration table if not already in there
-        table = "iter"+str(cur_iter)
-        print(table)
-        sql = 'CREATE TABLE IF NOT EXISTS {} ("epoch" int, "DPN_loss" numeric, "DPCNN_loss" numeric, "AC_loss" numeric, "FIFO_loss" numeric, "SJF_loss" numeric, "Random_loss" numeric, "Packer_loss" numeric, "Tetris_loss" numeric);'.format(table)
+        self.table = "iter"+str(cur_iter)
+        print(self.table)
+        sql = 'CREATE TABLE IF NOT EXISTS {} ("epoch" int, "dpn_loss" numeric, "dpcnn_loss" numeric, "ac_loss" numeric, "fifo_loss" numeric, "sjf_loss" numeric, "random_loss" numeric, "packer_loss" numeric, "tetris_loss" numeric);'.format(self.table)
         self.cur.execute(sql)
         self.conn.commit()
 
@@ -66,13 +88,13 @@ class DBconnect:
         self
 
     #VALUES must be in list form: [n_resources, syscap, %long, %short, resourcelow, resourcehigh, timelow, timehigh, n_jobs, n_epochs]
-    def updateparams(self, values):
+    def update_params(self, values):
         sql = 'SELECT count(*) FROM skynet_params WHERE iteration = %s;'
-        self.cur.execute(sql, (self.iter))
+        self.cur.execute(sql, (self.iter,))
         count = self.cur.fetchone()
         #print(count[0])
         if count[0] == 0:
-            sql = 'INSERT INTO skynet_params (iteration, resources, syscap, long, short, resourcelow, resourcehigh, timelow, timehigh, jobs, DPN_loss, DPCNN_loss, AC_loss, FIFO_loss, SJF_loss, Random_loss, Packer_loss, Tetris_loss) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 0, 0, 0, 0, 0, 0, 0, 0);'
+            sql = 'INSERT INTO skynet_params (iteration, resources, syscap, long, short, resourcelow, resourcehigh, timelow, timehigh, jobs, dpn_loss, dpcnn_loss, ac_loss, fifo_loss, sjf_loss, random_loss, packer_loss, tetris_loss) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 0, 0, 0, 0, 0, 0, 0, 0);'
             self.cur.execute(sql, (self.iter, values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8]))
             self.conn.commit()
             print("Parameters have been instantiated.")
@@ -84,15 +106,15 @@ class DBconnect:
 
     #OVERALL LOSSES must be in list form: [DPN_loss, DPCNN_loss, AC_loss, FIFO_loss, SJF_loss, Random_loss, Packer_loss, Tetris_loss]
     def update_master(self, losses):
-        sql = 'UPDATE skynet_params SET DPN_loss = %s, DPCNN_loss = %s, AC_loss = %s, FIFO_loss = %s, SJF_loss = %s, Random_loss = %s, Packer_loss = %s, Tetris_loss = %s WHERE iteration = %s;'
+        sql = 'UPDATE skynet_params SET dpn_loss = %s, dpcnn_loss = %s, ac_loss = %s, fifo_loss = %s, sjf_loss = %s, random_loss = %s, packer_loss = %s, tetris_loss = %s WHERE iteration = %s;'
         self.cur.execute(sql, (losses[0], losses[1], losses[2], losses[3], losses[4], losses[5], losses[6], losses[7], self.iter))
         self.conn.commit()
         print("Overall Losses have been updated.")
 
     #INTERMEDIATE LOSSES must be in list form: [DPN_loss, DPCNN_loss, AC_loss, FIFO_loss, SJF_loss, Random_loss, Packer_loss, Tetris_loss]
-    def updateiteration(self, losses, cur_epoch):
-        sql = 'INSERT INTO %s SET epoch = %s, DPN_loss = %s, DPCNN_loss = %s, AC_loss = %s, FIFO_loss = %s, SJF_loss = %s, Random_loss = %s, Packer_loss = %s, Tetris_loss = %s;'
-        self.cur.execute(sql, (self.iter, cur_epoch, losses[0], losses[1], losses[2], losses[3], losses[4], losses[5], losses[6], losses[7]))
+    def update_iteration(self, losses, cur_epoch):
+        sql = 'INSERT INTO {} (epoch, dpn_loss, dpcnn_loss, ac_loss, fifo_loss, sjf_loss, random_loss, packer_loss, tetris_loss) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);'.format(self.table)
+        self.cur.execute(sql, (cur_epoch, losses[0], losses[1], losses[2], losses[3], losses[4], losses[5], losses[6], losses[7]))
         self.conn.commit()
         print("Intermediate Losses have been updated.")
 
@@ -103,10 +125,14 @@ class DBconnect:
 #testing
 #get_last_iter()
 
-db = DBconnect(cur_iter=12)
+db = DBconnect(cur_iter=1)
 fake_params = [4, 3, 30, 70, .1, .5, 2, 15, 70, 50]
-fake_params[0]
-db.updateparams(fake_params)
+db.update_params(fake_params)
+
+fake_losses = [20, 18, 22, 45, 67, 12, 33, 59]
+db.update_master(fake_losses)
+
+db.update_iteration(fake_losses, 3)
 
 
 
